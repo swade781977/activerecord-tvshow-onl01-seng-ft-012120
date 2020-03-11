36994f5177a444146afd3ea78fb7c82c38fa3af8
Chris Wade <swade781977@yahoo.com>
2020-03-11 02:47:15 +0000

    Automatically backed up by Learn

---
+++ app/models/show.rb
@@ -0,0 +1,18 @@
+require 'pry'
+class Show < ActiveRecord::Base 
+  
+  
+  def self.highest_rating
+    s = Show.maximum('rating')
+  end
+  
+  def self.most_popular_show
+    s = Show.highest_rating
+    binding.pry
+    Show.where("rating = 's'")
+  end
+    
+
+
+
+end
